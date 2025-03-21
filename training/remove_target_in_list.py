a969137418b8092e6d06f7584f9815d8a95c5b57
jinseokim759 <boydaniel02@gmail.com>
2025-03-21 15:27:59 -0700

    Removing from list training

---
+++ training/remove_target_in_list.py
@@ -0,0 +1,15 @@
+nums = [1, 2, 3, 4, 5]
+
+def ex1(nums, target):
+    for i in range(len(nums)):
+        if target == nums[i]:
+            del nums[i]
+            return nums
+
+def ex2(nums, target):
+    nums.remove(target)
+    return nums
+
+        
+print(ex1(nums, 2))
+#print(ex2(nums, 2))
