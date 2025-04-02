8f40570383ee56482232f88b65705a29ebcd6d11
jinseokim759 <boydaniel02@gmail.com>
2025-04-01 17:43:28 -0700

    remove from list training

--- training/remove_target_in_list.py
+++ training/remove_target_in_list.py
@@ -1,9 +1,14 @@
-nums = [1, 2, 3, 4, 5]
+
+nums = [1, 2, 3, 4, 4, 5, 4, 4, 7, 8, 6, 6, 6, 4, 3]
 
 def ex1(nums, target):
-    for i in range(len(nums)):
-        if target == nums[i]:
-            del nums[i]
-            return nums
+    i, j = 0, 0
+    while i < len(nums):
+        if nums[i] != target:
+            nums[j] = nums[i]
+            j += 1
+        i += 1
+    del nums[j:]
+    return nums
         
-print(ex1(nums, 2))
+print(ex1(nums, 4))
