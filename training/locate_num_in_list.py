517eec03d5b744eba5fa9b02d75ab4c3f664d44d
jinseokim759 <boydaniel02@gmail.com>
2025-04-09 12:56:53 -0700

    find number in list training

--- training/locate_num_in_list.py
+++ training/locate_num_in_list.py
@@ -11,9 +11,7 @@ def locate_num(nums, num):
         else:
             right = mid - 1
     return -1
-            
-    
 
 print(locate_num(nums, 4))  #case1: num is in the nums
 print(locate_num(nums, 12)) #case2: num is bigger than numbers in nums
-print(locate_num(nums, 7)) #case3: num is smaller than the biggest number in nums, but is not in the nums
+print(locate_num(nums, 7))  #case3: num is smaller than the biggest number in nums, but is not in the nums
